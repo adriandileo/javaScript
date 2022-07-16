8ea62f221c4fc2200739eb7f3fe6aa134f37ca5a
adriandileo <adrian.dileo@bue.edu.ar>
2022-07-16 11:38:09 -0300

    primer commit

---
+++ js/java.js
@@ -0,0 +1,18 @@
+//clase 1
+
+//variables
+//  let edad = 26 //numero entero
+//  const nombre = "Cristian" //string
+
+//operaciones matematicas
+
+let num1 = 22
+let num2 = 41
+let mult = num1 * num2
+let text = "la multiplicacion"
+console.log(num1 + num2)
+console.log(text + mult)
+
+//pront
+let nombre = prompt("ingrese su nombre")
+console.log(nombre)
