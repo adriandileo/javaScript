1affd913b788f8c0de539217d2147e332d68f5ca
adriandileo <adrian.dileo@bue.edu.ar>
2022-07-29 11:23:36 -0300

    tarea2

--- js/tarea2.js
+++ js/tarea2.js
@@ -6,27 +6,33 @@ class personas{
         this.encargado = function(){
             switch(nombre,apellido){
                 case "Cristian", "Davino":
-                    console.log("Hola encargado "+ this.nombre+ " "+ this.apellido.toUpperCase())
+                    console.log("Hola encargado "+ this.nombre+ " "+ this.apellido.UpperCase())
                 default:
                     console.log("Hola cliente "+ this.nombre + " " + this.apellido)
                     break
             }
         }
     }  
-    
-    
-
+ 
+ 
 }
-
 const persona1 = new personas (prompt("ingrese nombre"),prompt("ingrese apellido"))    
 persona1.encargado()
 
-// for(const nombre in persona1){
-//     console.log(persona1)
-// }
+class comida{
+    constructor(producto,descripcion,precioBruto){
+        this.producto = producto
+        this.descripcion = descripcion
+        this.precioBruto = parseFloat(precioBruto)
+        }    
+    precioFinal(){
+        this.precioBruto = this.precioBruto * 1.21
+        console.log(this.precioBruto)
+    }
+}
+let comida1 = new comida (prompt("ingrese producto"),prompt("ingrese descripción"),prompt("ingrese precio bruto"))
 
 
 
-
-
-
+console.log(comida1)
+comida1.precioFinal()
