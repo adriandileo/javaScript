bbcd1bc8c97d024838f938709b3dbd1c6385cb77
adriandileo <adrian.dileo@bue.edu.ar>
2022-08-17 16:26:57 -0300

    final

--- js/final.js
+++ js/final.js
@@ -6,14 +6,12 @@ class Comida {
     constructor(nombre,descripcion,precio){
         this.nombre=nombre
         this.descripcion=descripcion
-        this.precio=precio
-    }
-    precioIva(){
-        precio= (precio+(precio*0,21))
-    }
+         this.precio=precio
+     }
+     precioIva(){
+         precio= (precio+(precio*0,21))
+     }
 }
-let comida=[]
-
 //boton!!!
 let agregar = document.getElementById("agregar")
 agregar.onclick= () => comida.push (new Comida (
@@ -22,18 +20,34 @@ agregar.onclick= () => comida.push (new Comida (
     +prompt("ingrese precio")));
 
 //resultado
+let comida=[{nombre: "tortilla francesa",descripcion: "Omellet de huevo con jamon y queso", precio: 200}]
 console.log(comida)
 console.dir(comida)
+let id = document.getElementById("id")
+let producto = document.getElementById("producto")
+let pedido = document.getElementById("pedido")
 
+const guardarLocal = (clave,valor) => { localStorage.setItem(clave, valor) };
+
+for (const comida1 of comida){
+    guardarLocal(comida1,JSON.stringify(id.innerText) )
+    console.log(comida1)
+}
+
+
+
+// for (const producto1 of comida){
+
+//     guardarLocal(producto1.nombre,JSON.stringify(comida))
+
+// }
+// console.log(producto1)
 //vista
 
-let divComida = document.getElementById("comida")
+let pedido1 = []
+let buttonPedido = document.getElementById("pedido")
+// buttonPedido.onclick = () => pedido.push ()
 
-
-
-for (const comidas of comida){
-    let li = document.createElement("li")
-    li.innerHTML=comidas
-    divComida.appendChild(li)
-    
-}
+// for (const id1 of id){
+//     guardar(id1, JSON.stringify(id))
+// }
