a084bfd5d3771fc7c6f14e4348b2b8dd64fa6dd3
adriandileo <adrian.dileo@bue.edu.ar>
2022-08-24 03:24:21 -0300

    Estructura basica final

--- js/final.js
+++ js/final.js
@@ -13,18 +13,16 @@ class Comida {
         
      }
 }
-
-//boton!!!
-let agregar = document.getElementById("agregar")
-agregar.onclick= () => comida.push (new Comida (
-    prompt("ingrese nombre"),
-    prompt("ingrese descripcion"),
-    +prompt("ingrese precio")));
-
+class Orden {
+    constructor (id){
+     this.nombre=id 
+    }
+    
+}
 //resultado
 let comida=[
-    {nombre: "tortilla francesa",descripcion: "Omellet de huevo con jamon y queso", precio: 300},
-    {nombre:"toston de masa madre", descripcion:"pan de masa madre con tapenade de tomates, queso y polvo de olivas", precio:300},
+    {nombre: "Tortilla francesa",descripcion: "Omellet de huevo con jamon y queso", precio: 300},
+    {nombre:"Toston de masa madre", descripcion:"Pan de masa madre con tapenade de tomates, queso y polvo de olivas", precio:300},
     {nombre:"Sanwinch Campirano",descripcion:"Tostado de jamon y queso de pan de masa madre",precio:300},
     {nombre:"Backroom Breackfast",descripcion:"Pan de masa madre con palta, huevo revuelto y panceta asada",precio:450},
     {nombre:"Mediterranea",descripcion:"Toston de masa madre con rucula, strachatella y tomates disecados",precio:450},
@@ -33,43 +31,35 @@ let comida=[
     // {nombre:"",descripcion:"",precio:},
     // {nombre:"",descripcion:"",precio:},
 ]
+let orden1 = []
 console.log(comida)
 console.dir(comida)
-
-let id = document.getElementById("id")
-let producto = document.getElementById("producto")
-let pedido = document.getElementById("pedido")
 const guardarLocal = (clave,valor) => { localStorage.setItem(clave, valor) };
-
 for (const comida2 of comida){
-    guardarLocal(comida2.nombre,JSON.stringify(comida2))
+    guardarLocal(comida2,JSON.stringify(comida2))
     console.log(comida2)
     let contenedor = document.createElement("div")
     contenedor.className="id"
     contenedor.innerHTML= comida2.nombre
     document.body,menu.appendChild(contenedor)
+
     let contenedor2 = document.createElement("div")
     contenedor2.className="producto"
     contenedor2.innerHTML= comida2.descripcion
     document.body,menu.appendChild(contenedor2)
+
     let contenedor3 = document.createElement("div")
     contenedor3.className="precio"
     contenedor3.innerHTML= comida2.precio
     document.body,menu.appendChild(contenedor3)
+
     let btn=document.createElement("button")
     btn.className="pedido"
     btn.id=comida2.nombre
     btn.innerHTML="+"
     document.body,menu.appendChild(btn)
-
-    
+    let orden =  document.getElementById(comida2.nombre)    
+    orden.onclick= () => orden1.push(new Orden(comida2.nombre))
 }
-
-let pedido1 = []
-let buttonPedido = document.getElementById("pedido")
-// buttonPedido.onclick = () => pedido.push ()
-
-// for (const id1 of id){
-//     guardar(id1, JSON.stringify(id))
-// }
-
+console.log(orden1)
+console.dir(orden1)
