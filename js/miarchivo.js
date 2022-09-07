9aec73ee5a7f1f836f874130c7fc025360a3a07b
adriandileo <adrian.dileo@bue.edu.ar>
2022-09-07 17:20:35 -0300

    error

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,1 +1,19 @@
-console.log(fetch("my-json-server.typicode.com/adriandileo/java/post"))
+// fetch("https://my-json-server.typicode.com/adriandileo/java/db")
+//     .then((response) => response.json())
+//     .then((json) => console.log(json));
+
+
+console.log(fetch("https://my-json-server.typicode.com/adriandileo/java"))
+
+
+// {
+//     "posts": [
+//       {
+//         "id": 1,
+//         "title": "hello"
+//       }
+//     ],
+//     "profile": {
+//       "name": "typicode"
+//     }
+// https://my-json-server.typicode.com/adriandileo/java/db  }
