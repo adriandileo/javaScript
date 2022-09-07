de74418d260b194de14d02a09a5f437fbfa4ac4d
adriandileo <adrian.dileo@bue.edu.ar>
2022-09-07 19:38:29 -0300

    anonymous

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,19 +1,33 @@
-// fetch("https://my-json-server.typicode.com/adriandileo/java/db")
-//     .then((response) => response.json())
-//     .then((json) => console.log(json));
+class Alcohol {
+    constructor(nombre,descripcion,precio){
+        this.nombre=nombre
+        this.descripcion=descripcion
+        this.precio=precio
+     }
+     precioIva(){
+        this.precio= (this.precio+(this.precio*0,21))
+        
+     }
+}
+class Orden2 {
+    constructor (id){
+     this.nombre=id 
+    }
+    
+}
 
+//console.log(fetch("https://my-json-server.typicode.com/adriandileo/java/db"))
 
-console.log(fetch("https://my-json-server.typicode.com/adriandileo/java/archivos"))
+const lista = document.getElementById("alcohol")
 
+// console.log(fetch("my-json-server.typicode.com/adriandileo/java/posts/1"))
 
-// {
-//     "posts": [
-//       {
-//         "id": 1,
-//         "title": "hello"
-//       }
-//     ],
-//     "profile": {
-//       "name": "typicode"
-//     }
-// https://my-json-server.typicode.com/adriandileo/java/db  }
+fetch("https://my-json-server.typicode.com/adriandileo/java/db")
+    .then((alcohol) => alcohol.json())
+    .then((json) => console.log(json))
+    .then( (data) => {
+        console.log( data[1].nombre )
+        console.log( data[1].precio )
+        })
+;
+
