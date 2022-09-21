d5600c5449d63ce251e48dd6aea9f346480e2325
adriandileo <adrian.dileo@bue.edu.ar>
2022-09-21 09:58:36 -0300

    final

--- js/orden.js
+++
@@ -1,9 +0,0 @@
-
-for (const orden of orden1){
-    guardarLocal(orden,JSON.stringify(orden))
-    console.log(orden)
-    // let contenedor = document.createElement("div")
-    // contenedor.className="id"
-    // contenedor.innerHTML= comida2.nombre
-    // document.body.menu,comidas.appendChild(contenedor)
-}
